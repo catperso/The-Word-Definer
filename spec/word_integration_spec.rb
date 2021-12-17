96261e091f544b717455759a62f01c2806e2c079
Matt Casperson <mc.casperson@gmail.com>
2021-12-17 15:57:56 -0800

    add first integration spec for adding a new word

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -2,3 +2,13 @@ require('capybara/rspec')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('create a word path', {:type => :feature}) do
+  it('creates a word and enters the words page') do
+    visit('/words')
+    click_on('Add a new word!')
+    fill_in('new_word', :with => 'Epicodus')
+    click_on('Add the word!')
+    expect(page).to have_content('Epicodus')
+  end
+end
