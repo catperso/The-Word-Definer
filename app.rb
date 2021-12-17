d89c5031d1cbc6f13b72a3479c37def872c13d76
Matt Casperson <mc.casperson@gmail.com>
2021-12-17 11:48:49 -0800

    add new_word.erb and adjust layout of words.erb, and routes to add words to the website

--- app.rb
+++ app.rb
@@ -4,7 +4,22 @@ require './lib/word'
 require './lib/definition'
 also_reload 'lib/**/*.rb'
 
-get '/' do
+get('/') do
+  redirect to('/words')
+end
+
+get('/words') do
   @words = Word.all
   erb(:words)
+end
+
+get('/words/new') do
+  erb(:new_word)
+end
+
+post('/words') do
+  new_word = params[:new_word]
+  word = Word.new({word: new_word})
+  word.save
+  redirect to('/words')
 end
