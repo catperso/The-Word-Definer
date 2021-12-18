02055bab7f7efca95e54af902223226d19426cba
Matt Casperson <mc.casperson@gmail.com>
2021-12-17 16:29:52 -0800

    add fifth integration spec for deleting all words and definitions

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -3,6 +3,11 @@ require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
 
+before(:each) do
+  Word.clear
+  Definition.clear
+end
+
 describe('create a word path', {:type => :feature}) do
   it('creates a word and enters the words page') do
     visit('/words')
@@ -46,5 +51,17 @@ describe('delete a definition', {:type => :feature}) do
     visit("/word/#{word.id}/definition/#{definition.id}")
     click_on('Remove definition!')
     expect(page).to have_content("This word is undefined!")
+  end
+end
+
+describe('clear the pseudo databases', {:type => :feature}) do
+  it("removes all definitions and words and resets the ID counters") do
+    word = Word.new(word: "Banana")
+    word.save
+    definition = Definition.new({definition: "A freudian fruit", word_id: word.id})
+    definition.save
+    visit("/")
+    click_on('Remove ALL words and ALL definitions?')
+    expect(page).to have_content("No words have been added!")
   end
 end
