f688815a9b958091a5b6acc65bbe83885c2b54ca
Matt Casperson <mc.casperson@gmail.com>
2021-12-17 16:03:47 -0800

    add second integration spec for adding a definition to a word

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -12,3 +12,14 @@ describe('create a word path', {:type => :feature}) do
     expect(page).to have_content('Epicodus')
   end
 end
+
+describe('create a definition path', {:type => :feature}) do
+  it('goes to a word page and creates a definition') do
+    word = Word.new(word: "Banana")
+    word.save
+    visit("/word/#{word.id}")
+    fill_in('new_definition', :with => 'A freudian fruit')
+    click_on('Add a new definition!')
+    expect(page).to have_content('A freudian fruit')
+  end
+end
